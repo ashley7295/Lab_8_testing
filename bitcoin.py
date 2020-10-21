ae1f82abf475435c5a9bee9621f5e3d94d527e59
ashley7295 <avkellerman@yahoo.com>
2020-10-20 21:11:04 -0500

    4 of 4 tests pass

--- bitcoin.py
+++ bitcoin.py
@@ -3,15 +3,19 @@ from pprint import pprint
 
 
 def main():
-    data = get_response()
+    amount, value = convert_to_bitcoin()
+    print_exchange_rate(amount, value)
 
-    bitcoin_amount = get_bitcoin_amount()
+def convert_to_bitcoin():
+    data = get_exchange_rate_data()
+
     exchange_rates = get_dollars_exchange_rate(data)
+    bitcoin_amount = get_bitcoin_amount()
     bitcoin_value = calculate_bitcoin_amount(bitcoin_amount, exchange_rates)
 
-    print_exchange_rate(bitcoin_amount, bitcoin_value)
+    return bitcoin_amount, bitcoin_value
 
-def get_response():
+def get_exchange_rate_data():
     url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
     response = requests.get(url)
@@ -26,12 +30,13 @@ def get_bitcoin_amount():
 def get_dollars_exchange_rate(data):
 
     dollars_exchange_rate = data['bpi']['USD']['rate_float']
-
     return dollars_exchange_rate
+
 
 def calculate_bitcoin_amount(bitcoin_amount, exchange_rate):
     bitcoin_value = bitcoin_amount * exchange_rate
     return bitcoin_value
+
 
 def print_exchange_rate(bitcoin_amount, bitcoin_value):
     return print (f'{bitcoin_amount} Bitcoin is equivilent to ${bitcoin_value:,}')
--- test_bitcoin.py
+++ test_bitcoin.py
@@ -5,17 +5,53 @@ from unittest.mock import patch, call
 import bitcoin
 
 class TestBitcoin(TestCase):
+    test_response = {
+        "time": {
+            "updated": "Oct 20, 2020 15:03:00 UTC",
+            "updatedISO": "2020-10-20T15:03:00+00:00",
+            "updateduk": "Oct 20, 2020 at 16:03 BST"
+        },
+        "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
+        "chartName": "Bitcoin",
+        "bpi": {
+            "USD": {
+            "code": "USD",
+            "symbol": "&#36;",
+            "rate": "11,906.8495",
+            "description": "United States Dollar",
+            "rate_float": 11906.8495
+            },
+            "GBP": {
+            "code": "GBP",
+            "symbol": "&pound;",
+            "rate": "9,180.5382",
+            "description": "British Pound Sterling",
+            "rate_float": 9180.5382
+            },
+            "EUR": {
+            "code": "EUR",
+            "symbol": "&euro;",
+            "rate": "10,073.3614",
+            "description": "Euro",
+            "rate_float": 10073.3614
+            }
+        }
+    }
 
-#1 get_response() gets the .json data from the API url
 
-#2 get_bitcoin_amount() gets the user input 
+    @patch('bitcoin.get_exchange_rate_data', side_effect=[test_response])
+    def test_get_exchange_rate_data(self, mock_response):
+        response = bitcoin.get_exchange_rate_data()
+        self.assertEqual(response, self.test_response)
+
+
+    #get_bitcoin_amount() gets the user input 
     @patch('builtins.input', side_effect = [5])
     def test_get_bitcoing_amount(self, mock_input):
         amount = bitcoin.get_bitcoin_amount()
         self.assertEqual(5, amount)
-#3 get_dollars_exchange() gets the specific exchange rate from teh .json data
 
-#4 calculate_bitcoin_amount() preforms math calculation
+    #calculate_bitcoin_amount() preforms math calculation
     def test_calculate_bitcoin_amount(self):
         first_num = 1
         second_num = 2
@@ -23,11 +59,13 @@ class TestBitcoin(TestCase):
         expected_total = 2
         self.assertEqual(total, expected_total)
 
-#5 print_exchange_rate() returns print
+    #print_exchange_rate() returns print
     @patch('builtins.print')
     def test_print_exchange_rate(self, mock_print):
         bitcoin.print_exchange_rate(5, 10)
         mock_print.assert_called_once_with('5 Bitcoin is equivilent to $10')
+
+
 
 if __name__ == '__main__':
     unittest.main()
